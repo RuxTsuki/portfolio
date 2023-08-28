ceb461e4bd30d6526c71fa02090d53400da40722
SaiKisho <diegosferas@gmail.com>
2023-08-27 20:10:07 -0500

    🚀 Initial commit - PWA ready Icons

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    workboxOptions: {
+        include: ['**/*.{css,js,html,svg,png,ico,txt}']
+    }
+})
+
+const nextConfig = {
+    reactStrictMode: true,
+}
+
+module.exports = withPWA(
+    nextConfig
+)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +19,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 export default config
