32b9951130633b4f1a0c1a569e466a57c17fbba1
SaiKisho <diegosferas@gmail.com>
2023-08-29 13:26:26 -0500

    💄 Styles Paddings & screens

--- src/components/hoc/SectionWrapper.tsx
+++ src/components/hoc/SectionWrapper.tsx
@@ -13,7 +13,7 @@ export const SectionWrapper = (Component: any, idName: any) => {
                 initial='hidden'
                 whileInView={'show'}
                 viewport={{ once: true, amount: 0.25 }}
-                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+                className={`${styles.padding} px-6 py-10 md:px-16 md:py-16 max-w-7xl mx-auto relative z-0`}
             >
                 <span className='hash-span' id={idName}>
                     &nbsp;
