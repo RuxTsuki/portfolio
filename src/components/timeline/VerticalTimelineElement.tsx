005318da32089dfe6bfd8e6e557737740178fa8c
SaiKisho <diegosferas@gmail.com>
2023-08-29 09:47:42 -0500

    🧪 Test Stash

--- src/components/timeline/VerticalTimelineElement.tsx
+++ src/components/timeline/VerticalTimelineElement.tsx
@@ -1,4 +1,5 @@
 
+import './vertical-line-element.css';
 interface Props {
     children: React.ReactNode,
     contentStyle: React.CSSProperties,
@@ -11,7 +12,8 @@ interface Props {
 
 export const VerticalTimelineElement = ({ children, contentStyle, className, contentArrowStyle, date, iconStyle, icon }: Props) => {
     return (
-        <div>
+        <div className="vertical-line-element-bg ml-[60px] px-4 py-5 relative rounded-[.25em]">
+            <span className='px-[.8em] opacity-[.7] inline-block text-[.8125rem]'>{date}</span>
             {children}
         </div>
     )
