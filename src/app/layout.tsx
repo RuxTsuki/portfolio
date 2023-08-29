b0831723d4d6188884f6d1a2042e7a0f927c9508
SaiKisho <diegosferas@gmail.com>
2023-08-29 13:35:08 -0500

    ♻️ bg dark for html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-primary'>
       <body className={inter.className}>{children}</body>
     </html>
   )
